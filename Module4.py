eb73fe0324554137613a3e8bf26bb00c34233015
Gage Adkins <gaadkins2012@gmail.com>
2022-03-13 17:02:25 -0700

    error checking

--- Module4.py
+++ Module4.py
@@ -1,4 +1,5 @@
 import datetime, pathlib, os, netmiko, getpass
+from netmiko.ssh_exception import AuthenticationException, SSHException, NetMikoTimeoutException
 
 USER = input("Enter your username: ")
 PASS = getpass.getpass("Enter your Password: ")
@@ -11,9 +12,14 @@ openFile = open(backup, write)
 
 netDevices = {'ip': ip_address, 'username': USER, 'password': PASS, 'device_type': 'cisco_ios'}
 
-c = netmiko.ConnectHandler(**netDevices)
-
-output = c.send_command('show run')
-
-openFile.write(output)
-openFile.close()
+try:
+    c = netmiko.ConnectHandler(**netDevices)
+    output = c.send_command('show run')
+    openFile.write(output)
+    openFile.close()
+except (AuthenticationException):
+    print("An authentication error occured while connecting to:", netDevices['ip'])
+except (SSHException):
+    print("An error occured while connecting to devivce")
+except (NetMikoTimeoutException):
+    print("The device timed out while trying to connect")
